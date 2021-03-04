e9b2edd92bf5c5fd1d2ac23f9e014dfc3bcfff21
kali <kali@kali>
2021-03-04 14:35:45 -0500

    CBC support added for files and 1024 bytes blocks

--- modes.h
+++ modes.h
@@ -9,12 +9,6 @@
 
 #include "aes.h"
 
-#define ERR_NF -1 // Input File does not exist 
-#define ERR_FEX -2 // Output file with this name already exists
-#define ERR_KSZ -3 // Invalid key size provided
-#define ERR_MD -4 // Invalid mode provided
-#define ERR_BSZ -5 // Invalid block size provided
-
 #define CBC 0
 
 ssize_t cbc_enc(const AES params, word* w, unsigned char iv[4*Nb], unsigned char block[1024], ssize_t blockSize);
